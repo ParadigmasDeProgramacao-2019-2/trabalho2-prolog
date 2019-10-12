875a2b9539a2b3ca6080297435b0d7f768fa8946
Joberth Rogers <joberth.rogers18@gmail.com>
2019-10-12 17:58:06 -0400

    Added sign up of made disciplines feature
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- teste.pl
+++ teste.pl
@@ -1,4 +1,5 @@
 :- dynamic discipline/2.
+:- dynamic made_dis/1.
 
 % Facts
 load_facts :-
@@ -13,14 +14,54 @@ load_facts :-
     assertz(discipline(82, 78)),
     assertz(discipline(78, 203)).
 
+search_disc(Cod) :-
+    discipline(Cod, _);
+    discipline(_, Cod).
+
+verify_answer(Question, Function, Function2) :-
+    nl,nl,nl, write(Question),nl, nl,
+    read(Choice),
+    (
+        Choice == y -> 
+            Function
+        ;
+        Choice == n ->
+            fail
+        ;
+        writeln('OPÇÃO INVALIDA, DIGITE NOVAMENTE!!!'), nl, nl,
+        Function2
+    ).
+
+sign_new_discipline :- 
+    nl, nl, write('Digite o código da matéria feita: '),nl, nl,
+    read(Disc),
+    (
+        search_disc(Disc) ->
+            memorize(made_dis(Disc)),
+            write('\e[2J'),  % clear screen
+            write('Disciplina registrada com sucesso!'), nl, nl
+        ;
+            write('\e[2J'),
+            write('Não foi possível registrar a disciplina!'), nl, nl
+    
+    ),
+    verify_answer('Deseja cadastrar mais outra matéria? (y/n)', sign_new_discipline, sign_new_discipline).
+
+sign_made_disc :- 
+    nl,nl,tab(15), write('MWCosultas'),nl,nl,
+    verify_answer('Deseja cadastrar disciplina cursada? (y/n) ', sign_new_discipline, sign_made_disc);
+    true. 
+
 % start here
-menu:- nl,nl,tab(15), write('MWCosultas'),nl,nl,
+menu:- nl,nl,
+    sign_made_disc,
+    write('\e[2J'),  % clear screen
     tab(1), writeln('Opções:'),
     tab(1),writeln('1) Ordenação topologica: '),
     tab(1),writeln('2) Sair '),
-    read(Chose),
+    read(Choice),
     (   
-        Chose == 1 ->
+        Choice == 1 ->
             load_facts,
             write('\e[2J'),
             start_topsort,
@@ -28,10 +69,10 @@ menu:- nl,nl,tab(15), write('MWCosultas'),nl,nl,
             get0(_),
             write('\e[2J')
         ;
-            Chose == 2 ->
+            Choice == 2 ->
             fail
         ;
-            writeln('INPUT INVALIDO, DIGITE NOVAMENTE!!!'),
+            writeln('OPÇÃO INVALIDA, DIGITE NOVAMENTE!!!'),
             write('\e[2J'),
             menu
     ), 
@@ -44,10 +85,10 @@ forget(X):-
 forgetAux(X):-
     retract(X).
     
-% memoriza(X):-
-%     forget(X), assert(X).
-%memoriza(X):-
-    %assertz(X).
+memorize(X):-
+     forget(X), assertz(X).
+memorize(X):-
+    assertz(X).
 
 get_next(Cod):-
     discipline(Cod, Prox),
